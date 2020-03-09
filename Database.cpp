afc0d2f53e50a13ee1e9d7ccbeb346782486b8a5
Suhyrhasan <Suhyrhasan@users.noreply.github.com>
2020-03-09 15:54:45 -0700

    done

--- Database.cpp
+++ Database.cpp
@@ -1,18 +1,20 @@
-#include "Database.h"
+#include "database.h"
 
-// Constructor
-// Parameterized constructor
-Database::Database(){}
-void Database::conClose()
+dataBase::dataBase()
+{
+
+}
+
+void dataBase::conClose()
 {
     mySqLiteDb.close();
     mySqLiteDb.removeDatabase(QSqlDatabase::defaultConnection);
 }
 
-bool Database::conOpen()
+bool dataBase::conOpen()
 {
     mySqLiteDb = QSqlDatabase::addDatabase("QSQLITE");
-    mySqLiteDb.setDatabaseName("C:/sqlite3/ScrumDogs.db");
+    mySqLiteDb.setDatabaseName("/Users/alvarohernandez/ScrumDogs.db");
     if(!mySqLiteDb.open())
     {
         qDebug() << ("Database Not connected");
@@ -24,58 +26,3 @@ bool Database::conOpen()
         return true;
     }
 }
-// Add Souvenirs (all fields at once)
-bool Database::AddSouvenirs(QString college, QString traditionalSouvenir, QString cost)
-{
-    QSqlQuery query;
-
-    // Prep "Add Souvenirs" Query
-    query.prepare("INSERT INTO souvenirs (college, traditionalSouvenir, cost) VALUES (:college, :traditionalSouvenir, :cost )");
-
-    // Bind safe values
-    query.bindValue(":college", college);
-    query.bindValue(":traditionalSouvenir", traditionalSouvenir);
-    query.bindValue(":cost", cost);
-
-    // Execute query and return status
-    return query.exec();
-}
-
-// Edit Souvenirs Data (individual fields)
-bool Database::UpdateSouvenirs(QString college, QString traditionalSouvenir, QString cost)
-{
-    QSqlQuery query;
-
-    // Prepare query to update customer listing
-    query.prepare("UPDATE souvenirs "
-                  "SET college = college, "
-                  "cost = :cost "
-                  "WHERE traditionalSouvenir = :traditionalSouvenir;");
-
-    // Bind safe values
-    query.bindValue(":college", college);
-    query.bindValue(":traditionalSouvenir", traditionalSouvenir);
-    query.bindValue(":cost", cost);
-
-    // Execute query and return status
-    return query.exec();
-}
-
-// Remove Souvenirs
-// Note: This needs to convert the input to uppercase
-bool Database::DeleteSouvenirs(QString traditionalSouvenir)
-{
-    QSqlQuery query;
-
-    // Prepare query to delete listing
-    query.prepare("DELETE from souvenirs "
-                  "WHERE traditionalSouvenir = :traditionalSouvenir;");
-
-    // Bind safe values
-    query.bindValue(":traditionalSouvenir", traditionalSouvenir);
-
-    return query.exec();
-}
-
-// Destructor
-Database::~Database() {}
--- Database.h
+++ Database.h
@@ -2,29 +2,16 @@
 #define DATABASE_H
 
 #include <QtSql>
-#include <QSqlQuery>
-#include<QMessageBox>
 #include<QSqlDatabase>
-class Database : public QSqlDatabase
+
+class dataBase
 {
 public:
-    // Constructor
-    Database();
+    dataBase();
     void conClose();
     bool conOpen();
-                // Add Souvenirs
-                // Note: This will convert empty values to null too
-    bool AddSouvenirs(QString college, QString traditionalSouvenir, QString cost);
-
-            // Edit Souvenirs Data
-    bool UpdateSouvenirs(QString college, QString traditionalSouvenir, QString cost);
-
-            // Remove Souvenirs
-    bool DeleteSouvenirs(QString traditionalSouvenir);
-
-    // Destructor
-    ~Database();
 private:
     QSqlDatabase mySqLiteDb;
 };
+
 #endif // DATABASE_H
