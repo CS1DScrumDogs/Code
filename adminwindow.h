251227d811f36975d191bcaa9649de9b5c96a845
Suhyrhasan <51502072+Suhyrhasan@users.noreply.github.com>
2020-03-09 16:17:25 -0700

    Revert "Suhyr's branch"

--- adminwindow.h
+++ adminwindow.h
@@ -1,16 +1,15 @@
 #ifndef ADMINWINDOW_H
 #define ADMINWINDOW_H
 
-#include <QtSql>
 #include <QDialog>
+#include <QtSql>
 #include <QtDebug>
 #include <QFileInfo>
-#include <QSqlQuery>
-#include <QTableView>
 #include <QMessageBox>
-#include<QSqlDatabase>
-#include "Database.h"
-namespace Ui {class AdminWindow;}
+namespace Ui {
+class AdminWindow;
+}
+
 class AdminWindow : public QDialog
 {
     Q_OBJECT
@@ -18,27 +17,11 @@ class AdminWindow : public QDialog
 public:
     explicit AdminWindow(QWidget *parent = nullptr);
     ~AdminWindow();
-public:
-    QSqlDatabase mySqLiteDb;
-    void InitializeTableView(QTableView *tableView);
-    bool IsAnyEmptyField();
 private slots:
     void on_pushButton_BackToMainWindow_clicked();
 
-    void on_pushButton_AddNewSouvenir_clicked();
-
-    void on_pushButton_DeleteOldSouvenir_clicked();
-
-    void on_pushButton_DisplayCurrentSouvenirs_clicked();
-
-    void on_pushButton_UpdateSouvenir_clicked();
-
-    void on_tableViewCurrentTraditionalSouvenirs_activated(const QModelIndex &index);
-
 private:
     Ui::AdminWindow *ui;
-    QSqlDatabase db;
-    Database *database;
 };
 
 #endif // ADMINWINDOW_H
